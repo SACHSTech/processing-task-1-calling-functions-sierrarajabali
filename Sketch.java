696a5291a200820730f5703397cac128599e3e96
sierrarajabali <sierra.rajabali26@ycdsbk12.ca>
2024-02-21 13:45:02 -0500

    done

--- Sketch.java
+++ Sketch.java
@@ -15,22 +15,39 @@ public class Sketch extends PApplet {
    * Called once at the beginning of execution.  Add initial set up
    * values here i.e background, stroke, fill etc.
    */
+
+  // Background color set to blue
   public void setup() {
-    background(210, 255, 173);
+    background(138, 181, 237);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
+
+  // Drawing of the house
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	// The grass (foreground) of the background
+  fill(21, 153, 51);
+  rect(0, 250, 400, 400);  
+  
+  // The roof of the house
+  fill(150, 58, 38);
+  triangle(100, 200, 200, 80, 300,200);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  // Base of the house
+  fill(255, 77, 207);
+  rect(100, 200, 200, 200); 
+
+  // Door of the house
+  fill(150, 150, 150);
+  rect(130, 295, 70, 130); 
+
+  // Doorknob of the house
+  fill(255, 255, 48);
+  circle(150, 360, 20);
+
   }
   
-  // define other methods down here.
 }
